483e479170c4aef2e5bd19be59098dccf36442bf
Gu1ia <burangulovag2@gmail.com>
2023-03-31 12:16:38 -0400

    US 01 is done

--- src/test/java/com/library/steps/UserInfoStepDefs.java
+++ src/test/java/com/library/steps/UserInfoStepDefs.java
@@ -6,6 +6,8 @@ import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.junit.Assert;
 
+import java.util.List;
+
 public class UserInfoStepDefs {
 
     String actualUserCount;
@@ -46,5 +48,23 @@ public class UserInfoStepDefs {
         System.out.println("**********************************************");
 
 
+    }
+
+    List<String>actualList;
+    @When("Execute query to get all columns")
+    public void execute_query_to_get_all_columns() {
+
+        DB_Util.runQuery("select * from users");
+
+        actualList = DB_Util.getAllColumnNamesAsList();
+
+        System.out.println("actualList = " + actualList);
+
+    }
+
+    @Then("verify the below columns are listed in result")
+    public void verify_the_below_columns_are_listed_in_result(List<String> expectedList) {
+
+        Assert.assertEquals(expectedList,actualList);
     }
 }
